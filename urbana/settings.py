c006f593420773590c44a80e34403b5bcfd96726
artificodes <chinecheremmbah64@gmail.com>
2026-03-18 15:41:59 +0100

    Added media asset deletion

--- urbana/settings.py
+++ urbana/settings.py
@@ -139,6 +139,12 @@ else:
         }
     }
 
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 # =====================================================
 # Redis / Channels
 # =====================================================
